d9c15c3837f0a92d75a541c990e705779e651826
Renita808 <renita808@gmail.com>
2020-09-06 09:18:05 -0400

    Backup before modifying to add div for large photos

---
+++ js/photography.js
@@ -0,0 +1,39 @@
+//   let photoList = $(".photo_list")
+//
+//   $(".photo_list").on("click", function({
+//     $(".single_photo").attr("src", photoList.src)
+//   )
+// });
+// $(".photo_list").on("click", "a", function(event){
+//   $(".single_photo").attr("src", this.href);
+// });
+// $(".photo_list").click(function() {
+  // var clickSource = $(".photo_list").attr("src");
+  // var replaceSource = $(".single_photo").attr("src")
+  // $(".single_photo").attr("src", $(".photo_list").attr("src"));
+// });
+
+// $(".photo_list").click(function() {
+//   let img =$(this)
+  // var clickSource = $(".photo_list").attr("src");
+  // var replaceSource = $(".single_photo").attr("src")
+//   $(".single_photo").attr({src: img.attr("src")});
+// });
+
+// function changeImage(event) {
+//   var targetElement = event.target || event.srcElement;
+//   $(".single_photo")attr("src", targetElement.src)
+// }
+
+// function changeImage(event)
+// var targetElement = event.target || event.srcElement;
+// if (targetElement.tagName === "img"){
+// document.getElementById(".single_photo").src = targetElement.getAttribute("src")
+// }
+// 
+// $(document).ready(function(e){
+//   $(".photo_list").click(function(e){
+//     console.log("It works!")
+//     $(".single_photo").attr("src", $(this).attr("src"))
+//   })
+// });
